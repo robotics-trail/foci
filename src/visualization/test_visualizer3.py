d9cadbec6f0970b409c43daae808edd0c00a550c
carlosIMAT <112658387+carlosIMAT@users.noreply.github.com>
2026-01-15 17:56:19 +0100

    "fix: minor changes"

--- src/visualization/test_visualizer3.py
+++ src/visualization/test_visualizer3.py
@@ -10,11 +10,12 @@ def test_visualizer():
     obstacle_means = np.array(
         [
             [0.0, 0.0, 0.0],
-            [-0.35, -0.35, -0.375],
-            [0.35, -0.35, -0.375],
-            [-0.35, 0.35, -0.375],
-            [0.35, 0.35, -0.375],
+            [-0.35, -0.45, -0.575],
+            [0.35, -0.45, -0.575],
+            [-0.35, 0.45, -0.575],
+            [0.35, 0.45, -0.575],
             [1.0, 0.1, 1.15],
+            # [1.0, 0.25, 1.0],
         ]
     )
     obstacle_covs = np.array(
@@ -25,8 +26,14 @@ def test_visualizer():
             np.diag([0.05**2, 0.05**2, 0.37**2]),
             np.diag([0.05**2, 0.05**2, 0.37**2]),
             np.diag([0.12**2, 0.12**2, 0.12**2]),
+            # np.diag([0.12**2, 0.12**2, 0.12**2]),
         ]
     )
+
+    # obstacle_means = np.array([[1.0, 0.1, 1.15], [1.0, 0.25, 1.0]])
+    # obstacle_covs = np.array(
+    #     [np.diag([0.12**2, 0.12**2, 0.12**2]), np.diag([0.12**2, 0.12**2, 0.12**2])]
+    # )
 
     robot_cov = np.eye(3) * 0.2**2
 
@@ -39,7 +46,11 @@ def test_visualizer():
         robot_cov=robot_cov,
         num_control_points=12,
         num_samples=25,
-        weights={"jerk": 0.00001, "goal": 100.0, "obstacle": 0.01},
+        weights={
+            "jerk": 0.00001,
+            "goal": 100.0,
+            "obstacle": 0.01,
+        },  # TODO: CAMBIAR A QUE DEPENDE DE 1 PESO
         wmax=5.0,
         vmax=5.0,
         amax=5.0,
@@ -52,7 +63,8 @@ def test_visualizer():
 
     vis = Visualizer(planner.robot, robot_cov, curve)
     vis.visualize_goal(ee_goal, radius=0.05)
-    vis.visualize_obstacles(obstacle_means, obstacle_covs)
+    # vis.visualize_obstacles(table_means, table_covs, name="Table")
+    vis.visualize_obstacles(obstacle_means, obstacle_covs, color=(100, 255, 100))
     vis.visualize_trajectory()
 
 
