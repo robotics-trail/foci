882a9ba26a09865d2bc24c1dfe256b6b5e3c6b05
Carlos Martinez <202211532@alu.comillas.edu>
2026-01-19 11:11:43 +0100

    feature: Real Gaussian Splats Visualization

--- src/utils/ply.py
+++ src/utils/ply.py
@@ -21,3 +21,41 @@ def extract_splat_data(ply_file):
     opacities = np.ones((len(splat), 1))
 
     return means, covs, colors, opacities
+
+
+def extract_splat_data_2(ply_file):
+    plydata = ply.PlyData.read(ply_file)
+    splat = plydata["vertex"]
+
+
+    means = structured_to_unstructured(splat[["x", "y", "z"]])
+
+    SH_C0 = 0.28209479177387814
+    colors = 0.5 + SH_C0 * structured_to_unstructured(
+        splat[["f_dc_0", "f_dc_1", "f_dc_2"]]
+    )
+    opacities = 1.0 / (1.0 + np.exp(-splat["opacity"][:, None]))
+
+    scales = np.exp(
+        structured_to_unstructured(splat[["scale_0", "scale_1", "scale_2"]])
+    )
+
+    wxyzs = structured_to_unstructured(
+        splat[["rot_0", "rot_1", "rot_2", "rot_3"]]
+    )
+
+    Rs = np.zeros((len(splat), 3, 3))
+
+    for i in range(len(splat)):
+        Rs[i] = R.from_quat(
+            [wxyzs[i][1], wxyzs[i][2], wxyzs[i][3], wxyzs[i][0]]
+        ).as_matrix()
+
+    covs = np.einsum(
+        "nij,njk,nlk->nil",
+        Rs,
+        np.eye(3)[None, :, :] * scales[:, None, :] ** 2,
+        Rs,
+    )
+
+    return means, covs, colors, opacities
